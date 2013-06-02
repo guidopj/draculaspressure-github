8922260b2cbe47aabce67f001756f0f0b730e56f
GUIDO <guidopujadas@gmail.com>
2013-06-02 14:32:21 -0300

    buildeo de travis

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,4 +1,5 @@
 Given(/^i have:$/) do |table|
+  data = table.raw
   
 end
 
@@ -69,4 +70,6 @@ end
 Then(/^I should see "(.*?)"$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
+
+
 
