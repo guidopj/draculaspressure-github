c64fedc4d341aa467b90fd7edcee2117ba1d5511
Chalu <sergio.bogado.tpi@gmail.com>
2013-06-06 22:09:38 -0300

    pruebas

--- app/app.rb
+++ app/app.rb
@@ -17,7 +17,8 @@ module Draculaspressureapp
     
     configure :staging, :production do
       use OmniAuth::Builder do
-        provider :twitter,'SGGn3SkV4hrAy7Em8eyxQ','0rOsaRXCPGaHDIYhxrvEC5ARC1L18XcAaWgn1JcFsm4'
+        provider :twitter,ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_SECRET_KEY'] 
+#'SGGn3SkV4hrAy7Em8eyxQ','0rOsaRXCPGaHDIYhxrvEC5ARC1L18XcAaWgn1JcFsm4'
 #,ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_SECRET_KEY'] 
       end
       set :login_page, "/auth/twitter"
