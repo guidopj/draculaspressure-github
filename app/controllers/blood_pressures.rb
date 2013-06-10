d72e8b8831023aa80971a6019960d9276a0df356
Chalu <sergio.bogado.tpi@gmail.com>
2013-06-09 23:02:22 -0300

    mas cambios

--- app/controllers/blood_pressures.rb
+++ app/controllers/blood_pressures.rb
@@ -23,7 +23,7 @@ Draculaspressureapp::App.controllers :blood_pressures do
 		render 'blood_pressures/average'
   end
 
-  get :create do
+  post :create do
 		@min_value_message = ""
 		@min_value_message = ""
 		params[:blood_pressure]["date"] = DateTime.now.to_s
