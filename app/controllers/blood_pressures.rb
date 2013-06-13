56c3f9154827aeb13f846dbec4d65bc85b31e8a3
GUIDO <guidopujadas@gmail.com>
2013-06-13 02:30:48 -0300

    act

--- app/controllers/blood_pressures.rb
+++ app/controllers/blood_pressures.rb
@@ -28,11 +28,11 @@ Draculaspressureapp::App.controllers :blood_pressures do
 				@errorMessageAverage= ""
 				render 'blood_pressures/average'				
 			rescue WithoutElementsException::ThereIsNoRecordsInTheSpecifiedRangeOfDates
-				@errorMessageAverage= "No hay tomas de presion entre las fechas indicadas"
+				@errorMessageAverage= "No such records in the specified dates"
 				render 'blood_pressures/averageForm'
 			end
 		else
-			@errorMessageAverage= "Fechas invalidas, por favor siga este formato: yyyy-mm-dd"
+			@errorMessageAverage= "Invalid dates, please follow the convention: yyyy-mm-dd"
 			render 'blood_pressures/averageForm'
 		end
   end
