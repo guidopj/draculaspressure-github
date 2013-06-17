17d0ac0f91e150f5ebabb15cff2cfe65dd1c4c99
Chalu <sergio.bogado.tpi@gmail.com>
2013-06-16 21:03:57 -0300

    agregada memory database para travis

--- config/database.rb
+++ config/database.rb
@@ -15,7 +15,7 @@ DataMapper::Property::String.length(255)
 case Padrino.env
   when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "draculaspressureapp_development.db"))
   when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "draculaspressureapp_test.db"))
-  when :travis      then DataMapper.setup(:default, "mysql://root@localhost/draculaspressureapp_test")
+  when :travis      then DataMapper.setup(:default, "sqlite3::memory")
   when :staging     then DataMapper.setup(:default, ENV['DATABASE_URL'])
   when :production  then DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
